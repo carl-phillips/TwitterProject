ef4eb5225ff2a5b04285677e08042d73036d0207
Carl Phillips <36976465+carl-phillips@users.noreply.github.com>
2019-12-03 17:05:59 -0600

    Updated variable names and html pages.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -13,9 +13,9 @@ def gettweets(request):
     if form.is_valid():
         search = form.cleaned_data['search_term']
         result_type = form.cleaned_data['result_type']
-        date = form.cleaned_data['time_range']
+        date = form.cleaned_data['date']
         all_the_tweets = get_tweets(search, result_type, date)
-        return render(request, 'polls/tweets.html', {'it': all_the_tweets})
+        return render(request, 'polls/tweets.html', {'all_tweets': all_the_tweets})
     else:
         print("error")
 
